1b931a24643f6d7d4cca881ed3201427902022b8
ShaileshKumar007 <shailesh1429k@gmail.com>
2022-06-26 02:59:55 +0530

    deleting unsused files

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,13 +3,11 @@ const userRoutes = require("./users");
 const postRoutes = require("./posts");
 const courseRoutes = require("./courses");
 const authRoutes = require("./auth");
-const uploadRoutes = require("./upload");
 
 // routes
 router.use("/users", userRoutes);
 router.use("/posts", postRoutes);
 router.use("/courses", courseRoutes);
 router.use("/auth", authRoutes);
-router.use("/upload", uploadRoutes);
 
 module.exports = router;
